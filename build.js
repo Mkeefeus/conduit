9fb20d688cc52922ed90f2909a87c5a8ceb823b8
Malcolm Keefe <Mkeefeus@gmail.com>
2025-01-26 18:16:53 -0500

    Destructure, fix meta errors

---
+++ build.js
@@ -0,0 +1,94 @@
+const prompt = require('prompt');
+const fs = require('fs');
+
+const EXCLUDED_FOLDERS = ['node_modules', '.git', '.vscode'];
+
+const FOLDERS = fs
+  .readdirSync('./')
+  .filter((file) => fs.lstatSync(file).isDirectory() && !EXCLUDED_FOLDERS.includes(file));
+
+const getSort = (folder) => {
+  switch (folder) {
+    case 'subclass':
+      return (a, b) => {
+        return a.className.localeCompare(b.className) || a.name.localeCompare(b.name);
+      };
+    case 'subclassFeature':
+      return (a, b) => {
+        return (
+          a.className.localeCompare(b.className) ||
+          a.subclassShortName.localeCompare(b.subclassShortName) ||
+          a.level - b.level
+        );
+      };
+    case 'classFeature':
+      return (a, b) => {
+        return a.className.localeCompare(b.className) || a.level - b.level;
+      };
+    default:
+      return (a, b) => {
+        return a.name.localeCompare(b.name);
+      };
+  }
+};
+
+prompt.start();
+
+prompt.get(
+  {
+    properties: {
+      version: {
+        description: 'Enter new version number',
+        type: 'string',
+        pattern: /^\d+\.\d+\.\d+$/,
+        message: 'Version must be in the format x.y.z where x, y, and z are numbers',
+        required: true,
+      },
+    },
+  },
+  (err, result) => {
+    if (err) {
+      console.error(err);
+      return;
+    }
+
+    const version = result.version;
+    const newJson = {
+      $schema: 'https://raw.githubusercontent.com/TheGiddyLimit/5etools-utils/master/schema/brew-fast/homebrew.json',
+    };
+    const _meta = {
+      sources: [
+        {
+          json: 'TCP',
+          abbreviation: 'TCP',
+          full: 'The Conduit Package',
+          authors: ['Malcolm Keefe', 'Scott', 'Riley'],
+          convertedBy: ['Malcolm Keefe'],
+          version: '0.1.0',
+        },
+      ],
+      edition: 'classic',
+      dateAdded: 0,
+      dateLastModified: Math.round(Date.now() / 1000),
+    };
+    newJson['_meta'] = _meta;
+    FOLDERS.forEach((folder) => {
+      newJson[folder] = newJson[folder] || [];
+      const jsonFiles = fs.readdirSync(`./${folder}`, { recursive: true }).filter((file) => file.endsWith('.json'));
+      jsonFiles.forEach((file) => {
+        const fileContent = fs.readFileSync(`./${folder}/${file}`);
+        const obj = JSON.parse(fileContent);
+        newJson[folder].push(obj);
+      });
+      newJson[folder].sort(getSort(folder));
+    });
+    const newJsonContent = JSON.stringify(newJson, null, 4);
+    fs.writeFile('./TCP.json', newJsonContent, (err) => {
+      if (err) {
+        console.error(`Error writing file TCP.json: ${err}`);
+      } else {
+        console.log(`File TCP.json created successfully.`);
+      }
+    });
+  }
+);
